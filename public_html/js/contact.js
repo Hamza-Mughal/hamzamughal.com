7b2b6efc8baf420e74ccff76251a67cafda6308c
hamza <mughal@gxmconsulting.com>
2018-06-20 12:46:13 -0400

    recaptcha validation and sending an email

--- public_html/js/contact.js
+++ public_html/js/contact.js
@@ -1,49 +1,46 @@
-  let validForm = true;
-  let captchaFilled = false;
+let validForm = true;
+let captchaFilled = false;
+
 let validateForm = () => {
   console.log("Test");
-  try{
   const name = $("#name").val();
   const email = $("#email").val();
   const emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const emailtitle = $("#email-title").val();
-  const emailbody = $("#emailbody").val();
-  if(name === ""){
+  const emailbody = $("#body").val();
+  try {
+      if(name === ""){
+        validForm = false;
+      }
+       if(!(emailregex.test(email))){
+         console.log(email);
+        validForm = false;
+      }
+      if(emailtitle === ""){
+        validForm = false;
+      }
+      if(emailbody === ""){
+        validForm = false;
+      }
+    }
+  catch(e){
+    console.log(e);
     validForm = false;
-  }
-   if(!(emailregex.test(email))){
-    validForm = false;
-  }
-  if(emailtitle === ""){
-    validForm = false;
-  }
-  if(emailbody === ""){
-    validForm = false;
-  }
-  if(recaptcha === ""){
-    validForm = false;
-  }
-}
-catch(e){
-  console.log("Exception");
-  validForm = false;
-}
+    }
 
-if(validForm && captchaFilled){
-  console.log("Form filled out successfully");
-  console.log(validForm);
-  console.log(captchaFilled);
-  return true;
-}
-else{
-  alert("Looks like you didn't complete the form correctly...");
-  console.log(validForm);
-  console.log(captchaFilled);
-    return false;
-}
+  if(validForm && captchaFilled) {
+    console.log("Form filled out successfully");
+    return true;
+  }
+  else {
+    alert("Looks like you didn't complete the form correctly...");
+    console.log(validForm);
+    console.log(captchaFilled);
+    validForm = true;
+      return false;
+    }
 }
 
 var correctCaptcha = function(response) {
     captchaFilled = true;
-    console.log(captchaFilled);
-    }
+}
--- public_html/server.js
+++ public_html/server.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var app = express();
-
+var Request = require("request");
 app.use(express.static(__dirname + "/images/"));
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 require("dotenv").config();
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "hamzamughaldotcom@gmail.com",
+    pass: process.env.EmailPassword
+  }
+});
+
 console.log(process.env.FOO);
 app.get("/", function(req, res){
   res.setHeader("Content-Type", "text/html");
@@ -24,16 +35,50 @@ app.get("/html/:fileName", function(req, res){
 });
 
 app.post("/contact", function(req, res){
-  console.log(req.body.name);
+
+      let googleReq = "https://www.google.com/recaptcha/api/siteverify?secret="
+  + process.env.key + "&response=" + req.body["g-recaptcha-response"]
+  + "&remoteip=" + req.connection.remoteAddress;
+  Request(googleReq, function(error, resp, body){
+    body = JSON.parse(body);
+    console.log(body.success);
+    if(body.success === false){
+      res.setHeader("Content-Type", "text/html");
+      console.log("Didnt complete the recaptcha...");
+      return res.send("The contact form was not filled out correctly"
+        + ". Return to the contact page <a href='/contact'>here</a>.");
+    }
+  });
   if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
   {
-    console.log("Didnt complete the recaptcha...");
-    res.send("Mail Sent Successfully"
-      + ". Return to homepage <a href='/'>here</a>.");
-  }
-  console.log("Captcha Success");
 
+    res.setHeader("Content-Type", "text/html");
+    console.log("Didnt complete the recaptcha...");
+    res.send("The contact form was not filled out correctly"
+      + ". Return to the contact page <a href='/contact'>here</a>.");
+  }
+  else{
+    console.log("Captcha Success");
+    var mailOptions = {
+      from: req.body.email,
+      to: 'hmughal2@masonlive.gmu.edu',
+      subject: req.body["email-title"],
+      text: req.body.name + " (email: " + req.body.email
+      + ") has sent a new message: \n\n" + req.body.body
+    };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.setHeader("Content-Type", "text/html");
+  res.send("Your email has been sent successfully."
+  + ". Return to the home page <a href='/'>here</a>.");
+  }
 });
+
 app.get("/contact", function(req, res){
   res.setHeader("Content-Type", "text/html");
   res.sendFile("/html/" + "contact.html", {root: __dirname });
@@ -68,6 +113,7 @@ app.listen(3002, function () {
 });
 
 app.get("*", function(req, res){
+  res.status(404);
   res.setHeader("Content-Type", "text/html");
   res.sendFile("/html/error.html", {root: __dirname });
 });
